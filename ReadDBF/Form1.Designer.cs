b1e64ae6c57053b300ee238d29f60ab6d1bcce19
Mohammad Mehrnia <qermezkon@gmail.com>
2020-10-20 08:16:47 +0330

    Add project files.

---
+++ ReadDBF/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ReadDBF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 422);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(800, 28);
+            this.mnuMain.TabIndex = 2;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 24);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem2.Text = "Open";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem3.Text = "Save To CSV";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem4.Text = "Exit";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.mnuMain);
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+    }
+}
+
---
+++ ReadDBF/Form1.cs
@@ -0,0 +1,132 @@
+﻿using IranSystemConvertor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadDBF
+{
+    public partial class Form1 : Form
+    {
+        private DataTable DT;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable GetYourData(string fullPath)
+        {
+            DataTable YourResultSet = new DataTable();
+
+            OleDbConnection yourConnectionHandler = new OleDbConnection($"Provider=VFPOLEDB.1;Data Source={new FileInfo(fullPath).Directory};");
+            yourConnectionHandler.Open();
+
+            if (yourConnectionHandler.State == ConnectionState.Open)
+            {
+                string mySQL = $"select * from {new FileInfo(fullPath).Name}";
+
+                OleDbCommand MyQuery = new OleDbCommand(mySQL, yourConnectionHandler);
+                OleDbDataReader reader = MyQuery.ExecuteReader();
+                DataTable schemaTable = reader.GetSchemaTable();
+                foreach (DataRow row in schemaTable.Rows)
+                {
+                    foreach (DataColumn column in schemaTable.Columns)
+                    {
+                        if (column.ColumnName=="ColumnName")
+                        {
+                            YourResultSet.Columns.Add(new DataColumn(row[column].ToString()));
+                        }
+                    }
+                }
+                while (reader.Read())
+                {
+                    DataRow dataRow = YourResultSet.NewRow();
+                    for (int i = 0; i < YourResultSet.Columns.Count; i++)
+                    {
+                        try
+                        {
+                            if (((DataColumn)YourResultSet.Columns[i]).DataType == typeof(System.String))
+                            {
+                                dataRow[((DataColumn)YourResultSet.Columns[i])] = ConvertTo.UnicodeFrom(TextEncoding.Arabic1256, Convert.ToString(reader[i]));
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            dataRow[((DataColumn)YourResultSet.Columns[i])] = String.Empty;
+                        }
+                    }
+                    YourResultSet.Rows.Add(dataRow);
+                    Application.DoEvents();
+                }
+                reader.Close();
+                MyQuery.Clone();
+                yourConnectionHandler.Close();
+
+                MyQuery.Dispose();
+                yourConnectionHandler.Dispose();
+            }
+            return YourResultSet;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "DBF|*.dbf";
+            if (openFileDialog.ShowDialog()==DialogResult.OK)
+            {
+                DT = GetYourData(openFileDialog.FileName);
+                dataGridView1.DataSource = DT;
+            }
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            if (DT == null || DT.Rows.Count == 0) return;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = "CSV|*.csv";
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.Filter = "CSV|*.csv";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ConvertTo.ToCSV(DT, saveFileDialog.FileName);
+            }
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SaveConvertedFile(string fullPathName)
+        {
+            String line;
+            StreamReader sr = new StreamReader(fullPathName);
+            line = sr.ReadLine();
+            //Continue to read until you reach end of file
+            while (line != null)
+            {
+                //write the lie to console window
+                Debug.WriteLine(ConvertTo.UnicodeFrom(TextEncoding.Arabic1256, line));
+                //Read the next line
+                line = sr.ReadLine();
+            }
+            //close the file
+            sr.Close();
+        }
+    }
+}
